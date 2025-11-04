a3a82a747bb4b67462e2df0a2a23b6047e719b5e
amberghost-maker <amber.ghost06@gmail.com>
2025-11-04 14:15:53 +1100

    Add files via upload

---
+++ Animal.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+class Amimal(ABC):
+     def move(self):
+          pass
+class Human(Animal):
+     def move(self):
+          print('I can walk and run')
+class Snake(Animal):
+     def move(self):
+          print('I can crawl')
+class Dog(Animal):
+     def move(self):
+          print('I can bark')
