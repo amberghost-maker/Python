8c9788e9fa7aa34dedb9c86a52ccf26fb72b6059
amberghost-maker <amber.ghost06@gmail.com>
2026-04-11 10:32:39 +1000

    Add files via upload

---
+++ ROCK_PAPER_SCISSORS_SHOOT.py
@@ -0,0 +1,14 @@
+import time
+import random
+print("~~~ ROCK PAPER SCISSORS ~~~")
+time.sleep(1)
+print("3")
+time.sleep(1)
+print("2")
+time.sleep(1)
+print("1")
+time.sleep(1)
+print("ROCK PAPPER SCISSORS SHOOT")
+time.sleep(1)
+Elements = ["ROCK", "PAPER", "SCISSORS"]
+print(random.choice(Elements))
